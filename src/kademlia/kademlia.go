093003e94140ac825f79cb2afe312a9a40fff61b
YuxuanHe <heyuxuan@heyuxuandeMacBook-Air.local>
2015-04-16 20:11:12 -0500

    modify the structure of kademlia
    create Update function and complete find_contact function

--- src/kademlia/kademlia.go
+++ src/kademlia/kademlia.go
@@ -9,26 +9,41 @@ import (
 	"net"
 	"net/http"
 	"net/rpc"
-    "strconv"
+  "strconv"
 )
 
 const (
 	alpha = 3
 	b     = 8 * IDBytes
-	k     = 20
+	bucketsize = 20 // 做了少许修改，因为把常量名由 k->bucketsize
 )
 
 // Kademlia type. You can put whatever state you need in this.
+//我认为这是k桶的结构
+
 type Kademlia struct {
 	NodeID ID
-    SelfContact Contact
+  SelfContact Contact
+	Bucketsize int         //我新建了一个Bucketsize 属性
+	Other_Contacts []Contact	 //这是一个指向Contact数组的指针，即指向其他结点
 }
 
 
+//定义 Kademlia 结构的一个方法
+func (route_table *Kademlia) Update(id ID) {
+		fmt.Println("good enough") //测试代码
+		route_table.FindContact(id)
+}
+
+
+
+//我们需要新建一个routing table， 根据现有的id， bucket size 以及 等等（待添加）
 func NewKademlia(laddr string) *Kademlia {
 	// TODO: Initialize other state here as you add functionality.
 	k := new(Kademlia)
 	k.NodeID = NewRandomID()
+	k.Bucketsize = bucketsize	//设定每个bucket大小为20
+
 
 	// Set up RPC server
 	// NOTE: KademliaCore is just a wrapper around Kademlia. This type includes
@@ -54,6 +69,8 @@ func NewKademlia(laddr string) *Kademlia {
         }
     }
     k.SelfContact = Contact{k.NodeID, host, uint16(port_int)}
+		k.Other_Contacts = []Contact{}   // 初始化数组指针
+		k.Update(k.NodeID)	//测试代码
 	return k
 }
 
@@ -69,7 +86,14 @@ func (e *NotFoundError) Error() string {
 func (k *Kademlia) FindContact(nodeId ID) (*Contact, error) {
 	// TODO: Search through contacts, find specified ID
 	// Find contact with provided ID
+		for i := 0; i<len(k.Other_Contacts); i++ {
+			//用for 循环查找 Other_Contacts 中，是否含有 特定的id值
+			if nodeId == k.Other_Contacts[i].NodeID {
+				return &k.Other_Contacts[i], nil
+			}
+		}
     if nodeId == k.SelfContact.NodeID {
+				fmt.Println("Find myself") //测试代码
         return &k.SelfContact, nil
     }
 	return nil, &NotFoundError{nodeId, "Not found"}
